86aaed145ecda2d7e2729c7178b5420f6f7ee3fe
angelxtry <angelxtry@gmail.com>
2022-11-07 15:06:57 +0900

    prettier, eslint 설정 추가

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,3 +1,14 @@
-const HomePage = () => <div>Home</div>;
+const HomePage = () => (
+  <div className='hero min-h-screen'>
+    <div className='hero-overlay bg-opacity-60' />
+    <div className='hero-content text-center text-neutral-content'>
+      <div className='max-w-md'>
+        <p className='mb-5'>글로벌 이커머스 플랫폼 키워드 분석도구</p>
+        <h1 className='mb-5 text-5xl font-bold'>GOMI 키워드</h1>
+        <button className='btn-primary btn'>Get Started</button>
+      </div>
+    </div>
+  </div>
+);
 
 export default HomePage;
